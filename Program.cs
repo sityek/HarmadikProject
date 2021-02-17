52b6a3f642b30b5fa8126f2afb5af87bbd42484e
Sityek <sityu@live.com>
2021-02-17 19:59:06 +0100

    Változások leírása.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HarmadikProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
